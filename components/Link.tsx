84e67330d7bbb288732afc28965f1307019b1663
unvalley <kirohi.code@gmail.com>
2023-02-11 16:01:53 +0900

    chore: resolve invalid link with a child

--- components/Link.tsx
+++ components/Link.tsx
@@ -10,11 +10,7 @@ const CustomLink = ({
   const isAnchorLink = href && href.startsWith('#')
 
   if (isInternalLink) {
-    return (
-      <Link href={href}>
-        <a {...rest} />
-      </Link>
-    )
+    return <Link href={href} {...rest}></Link>
   }
 
   if (isAnchorLink) {
