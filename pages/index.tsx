5699dd2b7acf94b02b4517615fd7581dfe8575b3
unvalley <kirohi.code@gmail.com>
2023-06-11 17:13:39 +0900

    chore: whats up

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home({
                     <p
                         className={`mb-8 bg-clip-text text-4xl font-extrabold leading-[60px] tracking-tight text-transparent ${headingColorClass} md:text-6xl md:leading-[86px]`}
                     >
-                        What's up bro
+                        What's up
                     </p>
                     <p className="mt-4 mb-8">
                         This is an{" "}
