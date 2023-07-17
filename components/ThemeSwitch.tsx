146f2d226da8f4c3756f46dc6b6cfc1e3aa14867
unvalley <kirohi.code@gmail.com>
2023-07-17 17:36:31 +0900

    wip

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -27,6 +27,7 @@ const ThemeSwitch = () => {
                 fill="currentColor"
                 className="text-gray-900 dark:text-gray-100"
             >
+                <title>Theme Switch</title>
                 {mounted && (theme === "dark" || resolvedTheme === "dark") ? (
                     <path
                         fillRule="evenodd"
