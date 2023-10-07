72dcfbc6ef99ed6a1bcbaa9c1a4d67a97954e35e
unvalley <kirohi.code@gmail.com>
2023-10-07 22:21:13 +0900

    chore: update biome as a project involved

--- data/index.ts
+++ data/index.ts
@@ -1,14 +1,23 @@
 export const headerNavLinks = [
-    { href: '/blog', title: 'Blog' },
-    { href: '/projects', title: 'Projects' },
-    { href: '/about', title: 'About' },
-]
+    { href: "/blog", title: "Blog" },
+    { href: "/projects", title: "Projects" },
+    { href: "/about", title: "About" },
+];
 
 export const projectsData = [
+    // latest
     {
-        title: 'TabX',
-        description: `A simple and tiny tab management chrome extension (OSS). `,
-        imgSrc: '/static/images/tabx_.png',
-        href: 'https://tabx.app/',
+        title: "Biome",
+        description:
+            "One toolchain for your web project. I'm involved as a core contributor.",
+        imgSrc: "/static/images/biome-640x400.png",
+        href: "https://biomejs.dev",
     },
-]
+    {
+        title: "TabX",
+        description:
+            "A simple and tiny tab management chrome extension (OSS). ",
+        imgSrc: "/static/images/tabx_.png",
+        href: "https://tabx.app/",
+    },
+];
