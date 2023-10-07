27c4b431b0195183210b75784d37e253850ba436
unvalley <kirohi.code@gmail.com>
2023-10-07 21:45:27 +0900

    chore: update top recent posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,46 +48,41 @@ export default function Home({
                         's blog.
                     </p>
                 </div>
-                <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                <ul>
                     {!posts.length && "No posts found."}
                     {posts.slice(0, MAX_DISPLAY).map((post) => {
                         const { slug, date, title, summary, tags } = post;
                         return (
-                            <li key={slug} className="py-6">
-                                <article>
-                                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                                        <dl>
-                                            <dt className="sr-only">
-                                                Published on
-                                            </dt>
-                                            <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                                                <time dateTime={date}>
-                                                    {formatDate(date)}
-                                                </time>
-                                            </dd>
-                                        </dl>
-                                        <div className="space-y-3 xl:col-span-3">
-                                            <div>
-                                                <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                                                    <CustomLink
-                                                        href={`/blog/${slug}`}
-                                                        className="text-gray-900 dark:text-gray-100"
-                                                    >
-                                                        {title}
-                                                    </CustomLink>
-                                                </h2>
-                                                <div className="flex flex-wrap">
-                                                    {tags.map((tag) => (
-                                                        <Tag
-                                                            key={tag}
-                                                            text={tag}
-                                                        />
-                                                    ))}
-                                                </div>
+                            <li key={slug} className="py-4">
+                                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                                    <dl>
+                                        <dt className="sr-only">
+                                            Published on
+                                        </dt>
+                                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                                            <time dateTime={date}>
+                                                {formatDate(date)}
+                                            </time>
+                                        </dd>
+                                    </dl>
+                                    <div className="space-y-3 xl:col-span-3">
+                                        <div>
+                                            <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                                                <CustomLink
+                                                    href={`/blog/${slug}`}
+                                                    className="text-gray-900 dark:text-gray-100"
+                                                >
+                                                    {title}
+                                                </CustomLink>
+                                            </h3>
+                                            <div className="flex flex-wrap">
+                                                {tags.map((tag) => (
+                                                    <Tag key={tag} text={tag} />
+                                                ))}
                                             </div>
-                                            <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                                                {summary}
-                                            </div>
+                                        </div>
+                                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                                            {summary}
                                         </div>
                                     </div>
                                 </article>
