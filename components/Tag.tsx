16856c79776443d403b999841367dd472ca75f76
unvalley <kirohi.code@gmail.com>
2024-03-22 03:48:07 +0900

    refactor

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,17 +1,17 @@
-import kebabCase from "@/lib/utils/kebabCase";
+import { kebabCase } from "@/lib/utils/kebabCase";
 import Link from "next/link";
 
 type Props = {
-    text: string;
+	text: string;
 };
 
 export const Tag = ({ text }: Props) => {
-    return (
-        <Link
-            href={`/tags/${kebabCase(text)}`}
-            className="mr-3 text-sm font-medium text-primary-800 hover:text-primary-500 dark:hover:text-primary-400"
-        >
-            #{text.split(" ").join("-")}
-        </Link>
-    );
+	return (
+		<Link
+			href={`/tags/${kebabCase(text)}`}
+			className="mr-3 text-sm font-medium text-primary-800 hover:text-primary-500 dark:hover:text-primary-400"
+		>
+			#{text.split(" ").join("-")}
+		</Link>
+	);
 };
--- lib/utils/kebabCase.ts
+++ lib/utils/kebabCase.ts
@@ -1,5 +1,3 @@
 import { slug } from "github-slugger";
 
-const kebabCase = (str: string) => slug(str, true);
-
-export default kebabCase;
+export const kebabCase = (str: string) => slug(str, true);
