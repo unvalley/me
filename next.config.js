307e7e0762231391d00aec9bb92500b99faa5c5e
unvalley <kirohi.code@gmail.com>
2024-12-28 22:33:01 +0900

    fix: csp

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;
   style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
+  img-src * blob: data: https://www.google-analytics.com;
   media-src 'none';
-  connect-src *;
+  connect-src * https://www.google-analytics.com;
   font-src 'self';
 `
 
