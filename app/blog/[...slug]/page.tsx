8010b3a0d2b819bf0527f7870b0113cc77b9592c
unvalley <38400669+unvalley@users.noreply.github.com>
2025-07-09 00:07:40 +0900

    chore: try fix metadata (#22)

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -58,7 +58,7 @@ export async function generateMetadata(props: {
     const metadata = (await import(`../_articles/${slug}.mdx`)).metadata;
     return {
       title: metadata.title,
-      description: metadata.summary || metadata.description,
+      description: metadata.description,
     };
   } catch {
     return {
