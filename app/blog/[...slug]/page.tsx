7c5555447d992bb21dfbd0fca8e7f47e4103b61b
unvalley <kirohi.code@gmail.com>
2025-07-08 23:55:44 +0900

    chore: remove summary

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -58,7 +58,7 @@ export async function generateMetadata(props: {
     const metadata = (await import(`../_articles/${slug}.mdx`)).metadata;
     return {
       title: metadata.title,
-      description: metadata.description || metadata.summary,
+      description: metadata.description,
     };
   } catch {
     return {
